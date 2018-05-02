159e7ab153b14b1432e18f94d7c10a6f7617d895
Naga Ravi Chaitanya Elluri <naga.elluri@colorado.edu>
2018-05-02 10:10:12 -0400

    Push R2R result to respective test branch

--- build-scripts/mastervert.sh
+++ build-scripts/mastervert.sh
@@ -53,36 +53,39 @@ if [[ "${CONTAINERIZED}" != "true" ]] && [[ "${CONTAINERIZED}" != "TRUE" ]]; the
 	echo "RUNNING FIRST ITERATION OF MASTERVERTICAL WITH $FIRST_RUN_PROJECTS PROJECTS"
 	echo "---------------------------------------------------------------------------"
 	sed -i "/- num/c  \ \ \ \ - num: $FIRST_RUN_PROJECTS" /root/svt/openshift_scalability/config/golang/pyconfigMasterVertScalePause.yaml
+	sed -i "/basename/c  \ \ \ \   basename: cp" /root/svt/openshift_scalability/config/golang/pyconfigMasterVertScalePause.yaml
 	pbench-user-benchmark --pbench-post='/usr/local/bin/pbscraper -i $benchmark_results_dir/tools-default -o $benchmark_results_dir; ansible-playbook -vv -i /root/svt/utils/pbwedge/hosts /root/svt/utils/pbwedge/main.yml -e \'new_file='$benchmark_results_dir/out.json''' -- /root/svt/openshift_scalability/masterVertical.sh golang
 	# Move results
         if [[ "${MOVE_RESULTS}" == "true" ]]; then
                 pbench-move-results --prefix=mastervert_1000_projects
         fi
-	echo "Deleting the namespaces created"
-	for ((n=0;n<$FIRST_RUN_PROJECTS;n++)); do
-        	oc delete project c$n
-	done
-	echo "sleeping for 5 min for the cluster to settle"
-	sleep 5m
+	#echo "Deleting the namespaces created"
+	#for ((n=0;n<$FIRST_RUN_PROJECTS;n++)); do
+        #	oc delete project c$n
+	#done
+	#echo "sleeping for 5 min for the cluster to settle"
+	#sleep 10m
 	# replace number of projects for the second  run
         echo "RUNNING SECOND ITERATION OF MASTERVERTICAL WITH $SECOND_RUN_PROJECTS PROJECTS"
 	echo "-----------------------------------------------------------------------------"
 	sed -i "/- num/c  \ \ \ \ - num: $SECOND_RUN_PROJECTS" /root/svt/openshift_scalability/config/golang/pyconfigMasterVertScalePause.yaml
+	sed -i "/basename/c  \ \ \ \   basename: clusterproject" /root/svt/openshift_scalability/config/golang/pyconfigMasterVertScalePause.yaml
         pbench-user-benchmark --pbench-post='/usr/local/bin/pbscraper -i $benchmark_results_dir/tools-default -o $benchmark_results_dir; ansible-playbook -vv -i /root/svt/utils/pbwedge/hosts /root/svt/utils/pbwedge/main.yml -e \'new_file='$benchmark_results_dir/out.json''' -- /root/svt/openshift_scalability/masterVertical.sh golang
 	# Move results
         if [[ "${MOVE_RESULTS}" == "true" ]]; then
                 pbench-move-results --prefix=mastervert_2000_projects
         fi
-	echo "Deleting the namespaces created"
-        for ((n=0;n<$SECOND_RUN_PROJECTS;n++)); do
-                oc delete project c$n
-        done
-        echo "sleeping for 5 min for the cluster to settle"
-        sleep 5m
+	#echo "Deleting the namespaces created"
+        #for ((n=0;n<$SECOND_RUN_PROJECTS;n++)); do
+        #        oc delete project c$n
+        #done
+        #echo "sleeping for 5 min for the cluster to settle"
+        #sleep 10m
 	# replace number of projects for the third run
         echo "RUNNING THIRD ITERATION OF MASTERVERTICAL WITH $THIRD_RUN_PROJECTS PROJECTS"
 	echo "---------------------------------------------------------------------------"
 	sed -i "/- num/c  \ \ \ \ - num: $THIRD_RUN_PROJECTS" /root/svt/openshift_scalability/config/golang/pyconfigMasterVertScalePause.yaml
+	sed -i "/basename/c  \ \ \ \   basename: cl" /root/svt/openshift_scalability/config/golang/pyconfigMasterVertScalePause.yaml
         pbench-user-benchmark --pbench-post='/usr/local/bin/pbscraper -i $benchmark_results_dir/tools-default -o $benchmark_results_dir; ansible-playbook -vv -i /root/svt/utils/pbwedge/hosts /root/svt/utils/pbwedge/main.yml -e \'new_file='$benchmark_results_dir/out.json''' -- /root/svt/openshift_scalability/masterVertical.sh golang
 	if [[ $? != 0 ]]; then
                 echo "1" > /tmp/test_status
@@ -91,14 +94,14 @@ if [[ "${CONTAINERIZED}" != "true" ]] && [[ "${CONTAINERIZED}" != "TRUE" ]]; the
         fi
 	# Move results
         if [[ "${MOVE_RESULTS}" == "true" ]]; then
-                pbench-move-results --prefix=mastervert_3000_projects
+                pbench-move-results --prefix=mastervert_4000_projects
         fi
-	echo "Deleting the namespaces created"
-        for ((n=0;n<$THIRD_RUN_PROJECTS;n++)); do
-                oc delete project c$n
-        done
-        echo "sleeping for 5 min for the cluster to settle"
-        sleep 5m
+	#echo "Deleting the namespaces created"
+        #for ((n=0;n<$THIRD_RUN_PROJECTS;n++)); do
+        #        oc delete project c$n
+        #done
+        #echo "sleeping for 5 min for the cluster to settle"
+        #sleep 10m
 
         # Move results
 #	if [[ "${MOVE_RESULTS}" == "true" ]]; then
