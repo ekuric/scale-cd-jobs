c6c0297b57c307532454c338030522a5b95fa7ca
Naga Ravi Chaitanya Elluri <nelluri@redhat.com>
2018-11-07 10:29:45 -0500

    Run pbench-ansible for containerized pbench after creating the daemonset

--- build-scripts/setup_tooling.sh
+++ build-scripts/setup_tooling.sh
@@ -14,21 +14,20 @@ ansible-playbook -vvv --extra-vars "register_all_nodes=${REGISTER_ALL_NODES}" -i
 if [[ "${CONTAINERIZED}" == "true" ]] || [[ "${CONTAINERIZED}" == "TRUE" ]]; then
 	cd /root/svt/openshift_tooling/pbench
 	./setup_pbench_pods.sh
-else
-	echo "Running pbench ansible"
-	echo "----------------------------------------------------------"
-	#### use a local pbench copy till the pprof fix is merged
-	if [[ -d "/root/pbench" ]]; then
-		rm -rf /root/pbench
-	fi
-	git clone https://github.com/distributed-system-analysis/pbench.git /root/pbench
-	cd /root/pbench/contrib/ansible/openshift/
-	pbench-clear-tools
-	ansible-playbook -vv -i ${TOOLING_INVENTORY} pbench_register.yml
-	echo "Finshed registering tools, labeling nodes"
-	echo "----------------------------------------------------------"
-	echo "List of tools registered:"
-	echo "----------------------------------------------------------"
-	pbench-list-tools
-	echo "----------------------------------------------------------"
 fi
+echo "Running pbench ansible"
+echo "----------------------------------------------------------"
+if [[ -d "/root/pbench" ]]; then
+	rm -rf /root/pbench
+fi
+git clone https://github.com/chaitanyaenr/pbench.git /root/pbench
+cd /root/pbench/contrib/ansible/openshift/
+git checkout ports
+pbench-clear-tools
+ansible-playbook -vv -i ${TOOLING_INVENTORY} pbench_register.yml
+echo "Finshed registering tools, labeling nodes"
+echo "----------------------------------------------------------"
+echo "List of tools registered:"
+echo "----------------------------------------------------------"
+pbench-list-tools
+echo "----------------------------------------------------------"
